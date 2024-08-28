995ebe8219e10d6cf7c8aeb0253a43cbafe60359
huangming177 <huangming@imwarewh.com>
2024-08-29 00:53:44 +0800

    first commit

---
+++ src/test/java/com/example/xq/XqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
