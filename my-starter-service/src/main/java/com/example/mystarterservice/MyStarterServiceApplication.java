5d168b34961e88846fcc18ab34ee78158d742240
huangming177 <huangming@imwarewh.com>
2024-08-29 12:59:40 +0800

    FIRST

---
+++ my-starter-service/src/main/java/com/example/mystarterservice/MyStarterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mystarterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStarterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStarterServiceApplication.class, args);
+    }
+
+}
---
+++ my-starter-service/src/test/java/com/example/mystarterservice/MyStarterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystarterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStarterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
