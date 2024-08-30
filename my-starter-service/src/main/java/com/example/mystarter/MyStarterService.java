50f9dd4dafc2504cf70bd5a858d18e4a931fba14
huangming177 <huangming@imwarewh.com>
2024-08-30 21:27:19 +0800

    starter案例

--- my-starter-service/src/main/java/com/example/mystarter/MyStarterService.java
+++ my-starter-service/src/main/java/com/example/mystarter/MyStarterService.java
@@ -5,14 +5,14 @@ public class MyStarterService {
     private MyStarterProperties myStarterProperties;
 
     public String getDataBaseType(String name){
-        String str = "hello "+name+" ,your dbType is "+ myStarterProperties.getDbType();
+        String str = "hello1111 "+name+" ,your dbType is "+ myStarterProperties.getDbType();
         System.out.println(str);
         return str;
     }
 
     public String getDataBaseUrl(){
         String url = myStarterProperties.getUrl();
-        url = "the database's url here :"+url;
+        url = "the database's url here` :"+url;
         System.out.println(url);
         return url;
     }
